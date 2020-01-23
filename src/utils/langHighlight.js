c6c61a35df73ef2ba89df04fd2b49759c4d2f3e0
LinFeng1997 <244732635@qq.com>
2020-01-23 17:05:45 +0800

    chore：highlight 打包优化

--- src/utils/langHighlight.js
+++ src/utils/langHighlight.js
@@ -1,48 +1,8 @@
 // 缩小highlight包大小,按需引入,900kb->90kb
 import highlightjs from "highlight.js/lib/highlight";
 
-const languages = [
-  "bash",
-  "clojure",
-  "cpp",
-  "cs",
-  "css",
-  "dart",
-  "dockerfile",
-  "diff",
-  "erlang",
-  "go",
-  "gradle",
-  "groovy",
-  "haskell",
-  "java",
-  "javascript",
-  "json",
-  "julia",
-  "kotlin",
-  "lisp",
-  "lua",
-  "makefile",
-  "markdown",
-  "matlab",
-  "objectivec",
-  "perl",
-  "php",
-  "python",
-  "r",
-  "ruby",
-  "rust",
-  "scala",
-  "shell",
-  "sql",
-  "swift",
-  "tex",
-  "typescript",
-  "verilog",
-  "vhdl",
-  "xml",
-  "yaml",
-];
+/* global HLSLANGUAGES */
+const languages = HLSLANGUAGES;
 languages.forEach((langName) => {
   // Using require() here because import() support hasn't landed in Webpack yet
   const langModule = require(`highlight.js/lib/languages/${langName}`);
