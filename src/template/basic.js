641eba5967755c107ed364f43aadd60e6fb46521
Toa <496807117@qq.com>
2021-10-13 15:52:50 +0800

    update: 更新默认样式

--- src/template/basic.js
+++ src/template/basic.js
@@ -2,301 +2,310 @@ export default `/*默认样式，最佳实践*/
 
 /*全局属性*/
 #nice {
-  font-size: 16px;
-  color: black;
-  padding: 0 10px;
-  line-height: 1.6;
-  word-spacing: 0px;
-  letter-spacing: 0px;
-  word-break: break-word;
-  word-wrap: break-word;
-  text-align: left;
-  font-family: Optima-Regular, Optima, PingFangSC-light, PingFangTC-light, 'PingFang SC', Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-  /* margin-top: -10px; 解决开头空隙过大问题*/
+  padding: 0px;
+  color: #363b45;
+  counter-reset: section;
+  letter-spacing: 1.25px;
+  line-height: 1.9;
+  font-weight: normal;
+  font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Helvetica Neue", PingFangSC, PingFangTC;
 }
-
-/*段落*/
 #nice p {
   font-size: 16px;
-  padding-top: 8px;
-  padding-bottom: 8px;
-  margin: 0;
-  line-height: 26px;
-  color: black;
+  margin: 18px 0px;
+  color: #363b45;
+  font-weight: 400;
+  font-family: "Helvetica Neue", PingFangSC, PingFangTC;
+  word-wrap: break-word;
+  text-align: justify;
 }
-
-/*标题*/
-#nice h1,
-#nice h2,
-#nice h3,
-#nice h4,
-#nice h5,
-#nice h6 {
-  margin-top: 30px;
-  margin-bottom: 15px;
-  padding: 0px;
-  font-weight: bold;
-  color: black;
+#nice.markdown-desc p {
+  font-size: 14px;
+  color: #656e82;
+}
+#nice .table-of-contents a {
+  color: #363b45;
+}
+#nice ol, #nice ul {
+  margin: 18px 0;
+  font-family: "Helvetica Neue", PingFangSC, PingFangTC;
+}
+#nice ol li, #nice ul li {
+  list-style-position: outside;
+  padding: 4px 0;
+  margin-left: 22px;
+  font-size: 16px;
+}
+#nice ol li {
+  list-style: decimal;
+}
+#nice ul li {
+  list-style: disc;
 }
 #nice h1 {
-  font-size: 24px;
+  color: #000;
+  text-align: center;
+  font-weight: bold;
+  font-size: 26px;
+  margin-bottom: 30px;
+}
+#nice h1:after {
+  display: inline-block;
+  content: " ";
+  margin-left: 20px;
+  vertical-align: center;
+  border-top: 15px solid transparent;
+  border-bottom: 15px solid transparent;
+  border-right: 15px solid rgba(255, 245, 0, 0.7);
+}
+#nice h1:before {
+  display: inline-block;
+  content: " ";
+  margin-right: 20px;
+  vertical-align: bottom;
+  border-top: 15px solid transparent;
+  border-bottom: 15px solid transparent;
+  border-right: 15px solid rgba(0, 255, 229, 0.5);
 }
 #nice h2 {
-  font-size: 22px;
+  margin: 50px 10px 40px 10px;
+  font-size: 26px;
+  text-align: center;
+  font-weight: bold;
+  color: #1d2027;
+  line-height: 2;
+  padding-bottom: 15px;
+}
+#nice h2 .content {
+  padding: 5px 8px;
+}
+#nice h2:before {
+  counter-increment: section;
+  content: "0" counter(section);
+  display: block;
+  font-size: 50px;
+  font-weight: 200;
+  color: #0f1115;
+  margin-bottom: 0px;
 }
 #nice h3 {
-  font-size: 20px;
+  margin: 40px 0px 20px 0px;
+  font-weight: bold;
+  font-size: 18px;
+  color: #1d2027;
+  border-left: 4px solid #00ffe5;
+  padding-left: 10px;
 }
 #nice h4 {
-  font-size: 18px;
+  color: #363b45;
 }
-#nice h5 {
-  font-size: 16px;
-}
-#nice h6 {
-  font-size: 16px;
-}
-
-#nice h1 .prefix,
-#nice h2 .prefix,
-#nice h3 .prefix,
-#nice h4 .prefix,
-#nice h5 .prefix,
-#nice h6 .prefix {
-  display: none;
-}
-
-#nice h1 .suffix
-#nice h2 .suffix,
-#nice h3 .suffix,
-#nice h4 .suffix,
-#nice h5 .suffix,
-#nice h6 .suffix {
-  display: none;
-}
-
-/*列表*/
-#nice ul,
-#nice ol {
-  margin-top: 8px;
-  margin-bottom: 8px;
-  padding-left: 25px;
-  color: black;
-}
-#nice ul {
-  list-style-type: disc;
-}
-#nice ul ul {
-  list-style-type: square;
-}
-
-#nice ol {
-  list-style-type: decimal;
-}
-
 #nice li section {
-  margin-top: 5px;
-  margin-bottom: 5px;
-  line-height: 26px;
-  text-align: left;
-  color: rgb(1,1,1); /* 只要是纯黑色微信编辑器就会把color这个属性吞掉。。。*/
-  font-weight: 500;
+  color: #363b45;
 }
-
-/*引用*/
 #nice blockquote {
-  border: none;
+  border-left: 3px solid rgba(144, 169, 218, 0.2);
+  background: rgba(144, 169, 218, 0.05);
+  font-size: 17px;
+  padding: 5px 0 5px 10px;
+  margin: 20px 0px;
 }
-
-#nice .multiquote-1 {
-  display: block;
-  font-size: 0.9em;
-  overflow: auto;
-  overflow-scrolling: touch;
-  border-left: 3px solid rgba(0, 0, 0, 0.4);
-  background: rgba(0, 0, 0, 0.05);
-  color: #6a737d;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 20px;
-  padding-right: 10px;
-  margin-bottom: 20px;
-  margin-top: 20px;
+#nice blockquote p {
+  color: #363b45;
+  font-size: 17px;
+  margin: 18px 10px;
+  padding: 0px 10px 0 0;
 }
-
-#nice .multiquote-1 p {
-  margin: 0px;
-  color: black;
-  line-height: 26px;
+#nice a {
+  color: #5283f0;
 }
-
-#nice .multiquote-2 {
-  box-shadow: 1px 1px 10px rgba(0,0,0,0.2);
-  padding: 20px;
-  margin-bottom: 20px;
-  margin-top: 20px;
+#nice strong {
+  color: #283244;
+  font-weight: bold;
 }
-
-#nice .multiquote-3 {
-  box-shadow: 1px 1px 10px rgba(0,0,0,0.2);
-  padding: 20px;
-  margin-bottom: 20px;
-  margin-top: 20px;
+#nice em {
+  color: #283244;
+  font-style: normal;
 }
-
-#nice .multiquote-3 p {
-  text-align: center;
+#nice em strong {
+  color: #283244;
 }
-
-#nice .multiquote-3 h3 {
-  text-align: center;
+#nice del {
+  color: #283244;
 }
-
-#nice .table-of-contents a {
-  border: none;
-  color: black;
+#nice hr {
+  border-top: 2px solid rgba(144, 169, 218, 0.3);
+  margin: 20px 0;
+}
+#nice img {
+  margin: 25px 0;
+}
+#nice li code, #nice p code {
+  background: rgba(144, 169, 218, 0.1);
+  color: #345ab7;
+  font-size: 90%;
+  padding: 3px 5px;
+  border-radius: 2px;
+}
+#nice table tr td, #nice table tr th {
+  border: 1px solid rgba(144, 169, 218, 0.2);
+}
+#nice table tr th {
+  background: rgba(144, 169, 218, 0.05);
+}
+#nice table tr:nth-child(2n) {
+  background: rgba(144, 169, 218, 0.05);
+}
+#nice .footnote-word {
+  font-size: 17px;
   font-weight: normal;
 }
-
-/*链接*/
-#nice a {
-  text-decoration: none;
-  color: #1e6bb8;
-  word-wrap: break-word;
-  font-weight: bold;
-  border-bottom: 1px solid #1e6bb8;
+#nice .footnote-ref {
+  font-weight: normal;
+}
+#nice .footnote-item {
+  display: flex;
+}
+#nice .footnote-item p {
+  color: #283244;
 }
 
-/*加粗*/
-#nice strong {
-  font-weight: bold;
-  color: black;
-}
-
-/*斜体*/
-#nice em {
-  font-style: italic;
-  color: black;
-}
-
-/*加粗斜体*/
-#nice em strong {
-  font-weight: bold;
-  color: black;
-}
-
-/*删除线*/
-#nice del {
-  font-style: italic;
-  color: black;
-}
-
-/*分隔线*/
-#nice hr {
-  height: 1px;
-  margin: 0;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  border: none;
-  border-top: 1px solid black;
-}
-
-/*代码块*/
-#nice pre {
-  margin-top: 10px;
-  margin-bottom: 10px;
-}
-#nice pre code {
-  display: -webkit-box;
-  font-family: Operator Mono, Consolas, Monaco, Menlo, monospace;
-  border-radius: 0px;
-  font-size: 12px;
-  -webkit-overflow-scrolling: touch;
-}
-#nice pre code span {
-  line-height: 26px;
-}
-
-/*行内代码*/
-#nice p code,
-#nice li code {
-  font-size: 14px;
-  word-wrap: break-word;
-  padding: 2px 4px;
-  border-radius: 4px;
-  margin: 0 2px;
-  color: #1e6bb8;
-  background-color: rgba(27,31,35,.05);
-  font-family: Operator Mono, Consolas, Monaco, Menlo, monospace;
-  word-break: break-all;
-}
-
-/*图片*/
-#nice img {
-  display: block;
-  margin: 0 auto;
-  max-width: 100%;
-}
-
-/*图片*/
 #nice figure {
-  margin: 0;
-  margin-top: 10px;
-  margin-bottom: 10px;
+  margin: 25px 0 35px;
 }
-
-/*图片描述文字*/
-#nice figcaption {
-  margin-top: 5px;
+#nice figure img {
+  margin: 0;
+}
+#nice figure figcaption {
+  margin-top: 10px;
+  width: 100%;
+  font-size: 15px;
   text-align: center;
-  color: #888;
+  font-weight: 400;
+  color: #b3bac8;
+  font-weight: 400;
+  font-family: "Helvetica Neue", PingFangSC, PingFangTC;
+}
+#nice h1, #nice h2, #nice h3, #nice h4, #nice h5, #nice h6 {
+  margin-top: 30px;
+  margin-bottom: 15px;
+  font-weight: bold;
+  color: black;
+}
+.markdown-content h1, .markdown-content h2, .markdown-content h3, .markdown-content h4, .markdown-content h5, .markdown-content h6 {
+  margin-top: 20px;
+  color: #152137;
+}
+.markdown-content h1 {
+  font-size: 20px;
+  font-weight: 700;
+}
+.markdown-content h2 {
+  font-size: 18px;
+  font-weight: 700;
+}
+.markdown-content h3 {
+  font-size: 16px;
+  font-weight: 600;
+}
+.markdown-content h3 strong {
+  font-weight: 700;
+  font-size: 16px;
+  color: #1d2027;
+  border-left: 4px solid #00ffe5;
+  padding-left: 10px;
+}
+.markdown-content h4 {
+  font-size: 14px;
+  font-weight: 500;
+}
+.markdown-content h5 {
   font-size: 14px;
 }
-
-
-/*表格容器 */
-#nice .table-container{
-  overflow-x: auto;
+.markdown-content h6 {
+  font-size: 12px;
 }
-
-/*表格*/
-#nice table {
-  display: table;
-  text-align: left;
+.markdown-content p {
+  margin-top: 10px;
+  font-size: 14;
 }
-#nice tbody {
-  border: 0;
+.markdown-content p:first-child {
+  margin-top: 0px;
 }
-
-#nice table tr {
-  border: 0;
-  border-top: 1px solid #ccc;
-  background-color: white;
+.markdown-content a {
+  font-weight: 500;
+  color: #5283f0;
 }
-
-#nice table tr:nth-child(2n) {
-  background-color: #F8F8F8;
+.markdown-content hr {
+  margin: 10px 0;
 }
-
-#nice table tr th,
-#nice table tr td {
-  font-size: 16px;
-  border: 1px solid #ccc;
-  padding: 5px 10px;
-  text-align: left;
-}
-
-#nice table tr th {
+.markdown-content b {
   font-weight: bold;
-  background-color: #f0f0f0;
 }
-
-/* 表格最小列宽4个汉字 */
-#nice table tr th:nth-of-type(n),
-#nice table tr td:nth-of-type(n){
-  min-width:85px;
+.markdown-content ul {
+  margin: 10px 0;
 }
-
+.markdown-content img {
+  margin: 10px 0;
+}
+.markdown-content br {
+  margin-top: 20px;
+  display: block;
+  content: " ";
+}
+.markdown-content ul li {
+  list-style: disc inside;
+  font-size: 14px;
+}
+.markdown-content ol {
+  margin: 10px 0;
+}
+.markdown-content ol li {
+  list-style: decimal inside;
+}
+.markdown-content blockquote {
+  background: #eef0f3;
+  margin: 12px 12px 12px 0;
+  padding: 12px;
+  border-left: 2px solid #d5dbe4;
+}
+.markdown-content blockquote p {
+  color: #656e82;
+}
+.markdown-content .out-site-link {
+  padding-left: 28px;
+  position: relative;
+  background-color: #f6f8fb;
+  border-radius: 4px;
+  padding-right: 8px;
+  display: inline-block;
+  margin-right: 16px;
+  line-height: 20px;
+  font-size: 14px;
+  font-weight: 400;
+  color: #2367ff !important;
+}
+.markdown-content .out-site-link:before {
+  content: "";
+  background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M9.97154 8.99997C9.97154 10.5955 9.69321 12.0115 9.2669 13.0062C9.05329 13.5047 8.81435 13.8697 8.57958 14.1014C8.34738 14.3305 8.15227 14.3999 8.00013 14.3999C7.84799 14.3999 7.65287 14.3305 7.42068 14.1014C7.18591 13.8697 6.94696 13.5047 6.73335 13.0062C6.30704 12.0115 6.02871 10.5955 6.02871 8.99997C6.02871 7.40443 6.30704 5.98842 6.73335 4.9937C6.94696 4.49528 7.18591 4.13025 7.42068 3.89856C7.65287 3.6694 7.84799 3.6 8.00013 3.6C8.15227 3.6 8.34738 3.6694 8.57958 3.89856C8.81435 4.13025 9.05329 4.49528 9.2669 4.9937C9.69321 5.98842 9.97154 7.40443 9.97154 8.99997Z' stroke='%232367FF' stroke-width='1.2'/%3E%3Ccircle cx='7.99997' cy='8.99997' r='5.39997' stroke='%232367FF' stroke-width='1.2'/%3E%3Cpath d='M3 9H13' stroke='%232367FF' stroke-width='1.2'/%3E%3C/svg%3E%0A");
+  position: absolute;
+  z-index: 2;
+  width: 16px;
+  height: 16px;
+  position: absolute;
+  left: 8px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+.markdown-content.view-content br {
+  margin-top: 10px;
+}
+.markdown-content.view-content p {
+  letter-spacing: 0.6px;
+}
+/*
+* 追加的样式
+*/
+/* 页脚 */
 #nice .footnote-word {
   color: #1e6bb8;
   font-weight: bold;
